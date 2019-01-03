5e23ae1857c5e1abda94e51b2ba7df1437f33759
srracerx <40307499+srracerx@users.noreply.github.com>
2019-01-03 12:13:20 -0200

    Add files via upload
    -->The game decides which player is the first one to play (based on the rules book)
    -->The tiles are now distribuited between  the players
    -->The hexadecimal integers inside the struct are now converted do decimal integers.

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 typedef struct pedrinhas{
 	char numero;
 	char cor;
+	int inteiro;
 	struct pedrinhas* prox;
 	
 }PEDRA;
@@ -122,11 +123,83 @@ void embaralha(PEDRA** pilha, int num){  //Passa as pedras para um array; Embara
 	return;
 }
 
+void inteiro(PEDRA** pilha){
+	PEDRA* percorre;
+	
+	percorre = *pilha;
+	
+	while(percorre != NULL){
+		percorre->inteiro = strtol(&percorre->numero, NULL, 16);
+		
+		percorre = percorre->prox;
+	}  //Obs: vai ficar com lixo de memória no campo dos inteiros quando for coringa.
+	
+	return;
+}
+
+void distribui(PEDRA** pilha, int num_jogadores, PEDRA** jogadores, int p_um){
+    int i;
+	int j;
+	PEDRA* percorre;
+	PEDRA* novo;
+	
+	percorre = *pilha;
+	
+	for(i = 0; i < num_jogadores; i++){
+		j = 0;
+		
+		while(j < 14){
+		   novo = (PEDRA*)calloc(1, sizeof(PEDRA));
+			   
+		   if(novo == NULL){
+			  cabecalho();
+			  printf("\n\n##ERRO DE MEMÓRIA!!");
+			  getchar();
+			  exit(1);
+		   }
+			   
+		   novo->numero = percorre->numero;
+		   novo->cor = percorre->cor;
+		   novo->inteiro = percorre->inteiro;	   
+			   
+		   //(~FUNÇÃO PARA DELETAR~).
+			   
+		   novo->prox = jogadores[p_um - 1];
+		   jogadores[p_um - 1] = novo;
+			   
+		   j++;
+		   percorre = percorre->prox;
+		}
+		if(p_um < num_jogadores){	
+		   p_um++;
+		}
+		else{	//	 p_um >= num_jogadores			
+		   p_um = 1;
+		}
+    }
+	
+	return;
+}
+
+void imprime_pilha(PEDRA* pilha){  //**Apenas para TESTE**
+	cabecalho();
+	while(pilha != NULL){
+		printf("\nNUMERO: %d      COR: %c", pilha->inteiro, pilha->cor);
+		pilha = pilha->prox;
+	}
+	return;
+}
+
+void imprime_mao(PEDRA** jogadores){  //Imprime a mão dos jogadores.
+	cabecalho()
+}
+
 int main(void){		
 //Declaração de variáveis------------------------------------------------	
 	int num_jogadores;  //Guarda o número de jogadores que estão participando.
 	int i;
 	int num;  //Guarda o número de pedras na pilha.
+	int p_um;  //Indica qual jogador será o primeiro.
 	PEDRA** jogadores;  //Trata-se de um vetor de ponteiros. Esse vetor guarda listas encadeadas que corres pondem as mãos de cada jogador.
 	PEDRA* pilha;  //Aponta para o primeiro elemento da pilha de pedras.
 	
@@ -159,14 +232,73 @@ int main(void){
 	
 	embaralha(&pilha, num);  //Função para embaralhar.
 	
-//-----------------------------------------------------------------------
-//Cada jogador pega uma pedra do monte. Aquele que pegar a pedra com valor mais alto fica determinado como o primeiro a jogar.
-    //~~FUNÇÃO PARA DETERMINAR QUEM É O PRIMEIRO. 
+	inteiro(&pilha);  //Converte os números em hex na struct para inteiros.
 	
-//-----------------------------------------------------------------------
-//14 peças são distribuídas para cada jogador.
-	//~~FUNÇÃO PARA TRANSFERIR PEDRA DA PILHA PARA AS MÃOS (A PILHA RESULTANTE É O SHOPPING).
-
+//Cada jogador pega uma pedra do monte-----------------------------------
+    switch(num_jogadores){  //(--FALTA INCLUIR CASO DO CORINGA--)
+		case 2:
+			if(pilha->inteiro >= pilha->prox->inteiro){  //Considera-se que o jogador um é o primeiro a retirar uma carta do baralho. 
+				p_um = 1;
+			}
+			else{
+				p_um = 2;
+			}
+			
+			break;
+			
+		case 3:
+		    if((pilha->inteiro >= pilha->prox->inteiro) && (pilha->inteiro >= pilha->prox->prox->inteiro)){
+				p_um = 1;
+			}
+			
+			if((pilha->prox->inteiro > pilha->inteiro ) && (pilha->prox->inteiro > pilha->prox->prox->inteiro)){
+				p_um = 2;
+			}
+			
+			if((pilha->prox->prox->inteiro > pilha->inteiro ) && (pilha->prox->prox->inteiro > pilha->prox->inteiro)){
+				p_um = 3;
+			}
+			
+			break;
+			
+		case 4:
+		    if((pilha->inteiro >= pilha->prox->inteiro) && (pilha->inteiro >= pilha->prox->prox->inteiro) && (pilha->inteiro >= pilha->prox->prox->prox->inteiro)){
+				p_um = 1;
+			}
+			
+			if((pilha->prox->inteiro > pilha->inteiro ) && (pilha->prox->inteiro > pilha->prox->prox->inteiro) && (pilha->prox->inteiro > pilha->prox->prox->prox->inteiro)){
+				p_um = 2;
+			}
+			
+			if((pilha->prox->prox->inteiro > pilha->inteiro ) && (pilha->prox->prox->inteiro > pilha->prox->inteiro) && (pilha->prox->prox->inteiro > pilha->prox->prox->prox->inteiro)){
+				p_um = 3;
+			}		    
+			
+			if((pilha->prox->prox->prox->inteiro > pilha->inteiro) && (pilha->prox->prox->prox->inteiro > pilha->prox->inteiro) && (pilha->prox->prox->prox->inteiro > pilha->prox->prox->inteiro)){
+				p_um = 4;
+			}
+			
+			break;
+			
+		default:
+			cabecalho();
+			printf("\n\n##QUANTIDADE DE JOGADORES INVÁLIDA!!");
+			getchar();
+			exit(1);
+	}
+	
+	cabecalho();
+	printf("\n\n>>O JOGADOR %d começa!", p_um);
+	getchar();
+	getchar();  //Dois getchar (?)
+	
+	embaralha(&pilha, num);  //Função para embaralhar (de novo).
+	
+//14 peças são distribuídas para cada jogador----------------------------
+	
+	distribui(&pilha, num_jogadores, jogadores, p_um);
+	//imprime.mao(jogadores);
+    imprime_pilha(pilha);
 //-----------------------------------------------------------------------
 //Cada jogador deve jogar uma ou mais séries de no mínimo 30 pontos. Se não puder fazer isso, ele deve recolher uma pedra e passar a sua vez.
 
